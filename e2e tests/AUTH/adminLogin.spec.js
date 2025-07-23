99a3aea40706812d409bacba6b3ddaf2721340cb
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2025-07-23 18:34:56 +0600

    fix fixtureData get issue

--- e2e tests/AUTH/adminLogin.spec.js
+++ e2e tests/AUTH/adminLogin.spec.js
@@ -1,19 +1,13 @@
-import {ADMIN_LOGIN} from '../../support/apiConstants.js';
-const { test, expect } = require('@playwright/test');
-const config = require('../../playwright.config.js');
-const fixturData = require('../../fixtures/adminLoginData.json');
+import { test, expect } from '@playwright/test';
+import { ADMIN_LOGIN } from '../../support/apiConstants.js';
+import fixtureData from '../../fixtures/adminLoginData.json';
 
+const config = require('../../playwright.config.js');
 const BASE_URL = config.use?.BASE_URL;
 
-test('Admin sign-in', async ({ request }) => {
-
-    console.log(`Request URL: ${BASE_URL}${ADMIN_LOGIN}`);
-    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
-      data: {
-        email: 'rezwankabirrobin@gmail.com',
-        password: '12345678',
-      }
-    });
-
-    expect(response.status()).toBe(200);
+test('Super Admin sign-in', async ({ request }) => {
+  const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+    data: fixtureData.jsonData[0],
+  });
+  expect(response.status()).toBe(200);
 });
